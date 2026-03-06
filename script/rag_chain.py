0f9aec17d9874ca7baf6a17efd4af5f2898f0b94
KimIshimwe <kim.ishimwe@univ-tlse3.fr>
2026-03-06 15:16:32 +0100

    Implémentation de la chaîne RAG

---
+++ script/rag_chain.py
@@ -0,0 +1,62 @@
+import os
+from dotenv import load_dotenv
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEmbeddings, HuggingFaceEndpoint
+from langchain_community.vectorstores import Chroma
+from langchain_core.prompts import PromptTemplate
+from langchain_classic.chains import RetrievalQA
+
+load_dotenv()
+
+def start_rag():
+    
+    embeddings = HuggingFaceEmbeddings(
+        model_name = "sentence-transformers/all-MiniLM-L6-v2",
+        model_kwargs = {'device': 'cpu'}
+        
+    )
+    
+    # connexion db
+    db  = Chroma(persist_directory= "db/chroma_db", embedding_function=embeddings)
+    
+    # config de l'IA
+    repo_id = "mistralai/Mistral-7B-Instruct-v0.2"
+    
+    llm = HuggingFaceEndpoint(
+        repo_id = repo_id,
+        huggingfacehub_api_token = os.getenv("HUGGINGFACEHUB_API_TOKEN"),
+        temperature  = 0.5,
+        max_new_tokens = 512,
+        task = "text-generation",
+    )
+    llm = ChatHuggingFace(llm=llm)
+    
+    # création du prompt
+    template= """Utilise les extraits du document suivants pour répondre à la question à la fin.
+    Si la réponse ne se trouve pas dans les extraits, dis que tu ne sais pas.
+    Réponds en français.
+    
+    CONTEXTE: {context}
+    QUESTION: {question}
+    REPONSE: """
+    
+    prompt = PromptTemplate(template = template,input_variables = ["context", "question"])
+    
+    # création de la chaîne rag
+    qa_chain = RetrievalQA.from_chain_type(
+        llm = llm,
+        chain_type = "stuff",
+        retriever = db.as_retriever(search_kwargs = {"k":3}),
+        chain_type_kwargs = {"prompt": prompt}
+          
+        )
+    
+    # testce qu'on parle de NDADAYEn
+    query = "NDADAYE a été assasiné en quelle année ?"
+    print(f"\n--- Question: {query} ---")
+    response = qa_chain.invoke(query)
+    print("\n--- Réponse de l'IA ---")
+    print(response["result"])
+    
+if __name__ == "__main__":
+        start_rag()
+    
