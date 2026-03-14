65d874edda968f31391cfdea91f52534d1572f62
KimIshimwe <kim.ishimwe@univ-tlse3.fr>
2026-03-14 18:55:11 +0100

    Optimisation du temps d'inférence

--- script/rag_chain.py
+++ script/rag_chain.py
@@ -6,35 +6,40 @@ from langchain_core.prompts import PromptTemplate
 from langchain_classic.chains import RetrievalQA
 
 load_dotenv()
-
-def start_rag(query):
-    
-    embeddings = HuggingFaceEmbeddings(
+# initialisation des embeddings
+def init_embeddings():
+    return HuggingFaceEmbeddings(
         model_name = "Qwen/Qwen3-Embedding-0.6B",
         model_kwargs = {'device': 'cpu'}
         
     )
-    
-    # connexion db
-    db  = Chroma(persist_directory= "db/chroma_db", embedding_function=embeddings)
-    
-    # config de l'IA
+
+#initialisation de bd vectorielle
+def init_db(embeddings):
+    return Chroma(persist_directory= "db/chroma_db", embedding_function=embeddings)
+
+#initialisation du LLM
+def init_llm():
     repo_id = "mistralai/Mistral-7B-Instruct-v0.2"
     
-    llm = HuggingFaceEndpoint(
+    llm_endpoint = HuggingFaceEndpoint(
         repo_id = repo_id,
         huggingfacehub_api_token = os.getenv("HUGGINGFACEHUB_API_TOKEN"),
         temperature  = 0.5,
         max_new_tokens = 512,
         task = "text-generation",
     )
-    llm = ChatHuggingFace(llm=llm)
-    
-    # création du prompt
+
+    return ChatHuggingFace(llm=llm_endpoint)
+
+#génération de la réponse
+def get_answer(query,db,llm):
+     # création du prompt
     template= """Utilise les extraits du document suivants pour répondre à la question à la fin.
     Si la réponse ne se trouve pas dans le contexte, dis que tu ne sais pas.
     
-    Réponds en français et réponds uniquement ç la question ne donne pas de détails.
+    Réponds en français et réponds uniquement à la question ne donne pas de détails.
+    Sois factuel et utilise un ton professionnel.
     
     CONTEXTE: {context}
     QUESTION: {question}
@@ -48,24 +53,17 @@ def start_rag(query):
         chain_type = "stuff",
         retriever = db.as_retriever(search_kwargs = {"k":3}),
         chain_type_kwargs = {"prompt": prompt},
-        return_source_documents = True 
+        return_source_documents = False 
         )
     
     # testce qu'on parle de NDADAYEn
 
     #print(f"\n--- Question: {query} ---")
     response = qa_chain.invoke(query)
-    #print("\n--- Réponse de l'IA ---")
-    print(response["result"])
+    return {
+        "result": response["result"]
+    }
     
-    #print("\n--- Sources: ---")
-    sources = response["source_documents"]
-    for i, doc in enumerate(sources):
-        page = doc.metadata.get("page", "N/A")
-       # print(f"Source {i+1}: Page {page}")
-        
-    
-    
-if __name__ == "__main__":
-        start_rag("Quelle ethnie est majoritaire ?")
+   
+
     
