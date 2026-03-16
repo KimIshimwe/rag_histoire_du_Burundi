31b8c3591d4c15bf9e1306e01feb0adbc1319cf9
KimIshimwe <kim.ishimwe@univ-tlse3.fr>
2026-03-16 16:07:05 +0100

    Nouvelle base et vecteurs

--- script/indexing.py
+++ script/indexing.py
@@ -2,11 +2,11 @@ import os
 from dotenv import load_dotenv
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
-from sentence_transformers import SentenceTransformer
+
 
 load_dotenv()
 
-def create_vector_store(chunks, db_path = "db/chroma_db"):
+def create_vector_store(chunks, db_path = "db_burundi/chroma_db"):
     # si le dossier n'existe pas Qwen/Qwen3-Embedding-4B
     if not os.path.exists(db_path):
         os.makedirs(db_path)
@@ -28,7 +28,7 @@ def create_vector_store(chunks, db_path = "db/chroma_db"):
 if __name__ == "__main__":
     
     from ingestion import ingest_pdf
-    path_to_pdf = "data/history_of_Burundi.pdf"
+    path_to_pdf = "data"
     
     if os.path.exists(path_to_pdf):
         my_chunks = ingest_pdf(path_to_pdf)
