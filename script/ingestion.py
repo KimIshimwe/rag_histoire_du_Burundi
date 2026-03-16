21296ee8752a68bb9e0b04822d1907f737765553
KimIshimwe <kim.ishimwe@univ-tlse3.fr>
2026-03-16 17:23:06 +0100

    Traitement des tableaux dans le PDF

--- script/ingestion.py
+++ script/ingestion.py
@@ -1,33 +1,55 @@
-from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
+import fitz 
+import ollama
+from langchain_core.documents import Document
+from docling.document_converter import DocumentConverter
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-def ingest_pdf(path):
-    # chargement de tous les pdf dans le bon dossier
-    loader = DirectoryLoader(
-        path,
-        glob="./*.pdf",
-        loader_cls=PyPDFLoader
-    )
-    pages = loader.load()
-    
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size = 512,
-        chunk_overlap = 100, 
-        separators= ["\n\n", "\n", ". ", " ",""]
-    )
-    chunks = text_splitter.split_documents(pages)
-    
-    return chunks
+#DATA_DIR = "data/"
+IMAGE_DIR = "data/extracted_images"
 
-if __name__ == "__main__":
-    pdf = "data/history_of_Burundi.pdf"
-    if os.path.exists(pdf):
-        docs = ingest_pdf(pdf)
-        # premier morceau
-        print(docs[0].page_content + "...")
-    else:
-        print("Le fichier PDF pas retrouvé.")
+if not os.path.exists(IMAGE_DIR):
+    os.makedirs(IMAGE_DIR)
+    
+def get_image_description(image_path):
+    try:
+        res = ollama.chat(
+            model = 'llava',
+            messages = [{'role':'user', 'content': 'Décris précisément ce graphique ou cette image technique.', 'image': [image_path]}]
+        )
+        return res['message']['content']
+    
+    except Exception as e:
+        return f"Erreur description image: {e}"
+    
+def ingest_pdf(directory_path):
+    converter = DocumentConverter()
+    all_docs = []
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1500,
+                                                   chunk_overlap = 200, 
+                                                   separators= ["\n\n# ", "\n\n## ", "\n\n", "\n", " "]
+                                                    )
+    # parcourir tous les pdf
+    files = [f for f in os.listdir(directory_path) if f.endswith('.pdf')]
+    
+    for file_name in files:
+        file_path = os.path.join(directory_path, file_name)
+        
+        # A. conversion du pdf en objet structuré puis en markdown pour les tableaux
+        result = converter.convert(file_path)
+        markdown_text = result.document.export_to_markdown()
+        chunks = text_splitter.split_text(markdown_text)
+        for chunk in chunks:
+            all_docs.append(Document(page_content=chunk, metadata={"source":file_name, "type":"text"}))
+            
+    return all_docs
+       
+        
+        
+        
+        
+        
+   
